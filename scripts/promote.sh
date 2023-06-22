ad7fb7c35f4b7dbe384c73fa3409e78f4b397a1c
Hugh Saunders <hugh.saunders@cyberark.com>
2023-06-22 16:22:26 +0100

    CNJR-1442: move from maven release to maven deploy

--- scripts/promote.sh
+++ scripts/promote.sh
@@ -7,14 +7,14 @@ set -euo pipefail
 ensure_mvn
 set -x
 
-git config --global user.email "conj_ops@cyberark.com"
-git config --global user.name "CyberArk Conjur Jenkins"
+mvn --batch-mode deploy
 
-echo "==== Preparing Release ===="
-mvn --batch-mode release:prepare
+# echo "==== Preparing Release ===="
+# mvn --batch-mode release:prepare
 
-echo "==== Release Properties: ===="
-cat release.properties
+# echo "==== Release Properties: ===="
+# cat release.properties
 
-echo "==== Executing Release ===="
-mvn --batch-mode release:perform
+# echo "==== Executing Release ===="
+# mvn --batch-mode release:perform
+
