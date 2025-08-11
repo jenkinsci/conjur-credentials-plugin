53b828099f4e5781ac1b763b0a795057812c0af2
Konrad Kulbat <konrad.kulbat@cyberark.com>
2025-08-11 11:56:47 +0200

    CNJR-10551: Remove inline JS from conjur-credentials-plugin

--- src/main/webapp/scripts/configuration/GlobalConjurConfiguration/global-config.js
+++ src/main/webapp/scripts/configuration/GlobalConjurConfiguration/global-config.js
@@ -1,62 +1,60 @@
 function validateNumber(evt) {
-    var theEvent = evt || window.event;
-    var key;
+    const theEvent = evt || window.event;
+    let key;
 
-  if (theEvent.type === "paste") {
+    if (theEvent.type === 'paste') {
         key = event.clipboardData.getData('text/plain');
-  } else {
+    } else {
         key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode(key);
     }
-    var regex = /\d|\./;
+    const regex = /\d|\./;
     if (!regex.test(key)) {
         theEvent.returnValue = false;
-        if (theEvent.preventDefault) theEvent.preventDefault();
+        if (theEvent.preventDefault) {
+            theEvent.preventDefault();
+        }
     }
 }
 
 document.getElementById('listAuthenticator').addEventListener('change', changeAuthenticationOption);
 
 function getIdentityFormatToken() {
-  var listAuthenticator = document.getElementById("listAuthenticator");
-  var selectAuthenticator = listAuthenticator.dataset.authenticator;
-  var selectIdentityFormatTokenValue = listAuthenticator.dataset.token;
+    const listAuthenticator = document.getElementById('listAuthenticator');
+    const selectAuthenticator = listAuthenticator.dataset.authenticator;
+    let selectIdentityFormatTokenValue = listAuthenticator.dataset.token;
 
-  var selectedIdentityFormatFiledToken = document.getElementById("listIdentityFormatFieldsFromToken");
-  selectIdentityFormatTokenValue = selectIdentityFormatTokenValue ? selectIdentityFormatTokenValue : "jenkins_full_name";
-  selectedIdentityFormatFiledToken.value = selectIdentityFormatTokenValue;
+    const selectedIdentityFormatFiledToken = document.getElementById('listIdentityFormatFieldsFromToken');
+    selectIdentityFormatTokenValue = selectIdentityFormatTokenValue || 'jenkins_full_name';
+    selectedIdentityFormatFiledToken.value = selectIdentityFormatTokenValue;
 
-
-  for (var j = 0; j < listAuthenticator.length; j++) {
-      if (listAuthenticator.options[j].value == selectAuthenticator) {
-          listAuthenticator.options[j].selected = true;
-      }
-  }
+    for (let j = 0; j < listAuthenticator.length; j++) {
+        if (listAuthenticator.options[j].value === selectAuthenticator) {
+            listAuthenticator.options[j].selected = true;
+        }
+    }
 }
 
 getIdentityFormatToken();
 
 function changeAuthenticationOption() {
-    var listAuthenticator = document.getElementById('listAuthenticator');
-    var optionText = listAuthenticator.options[listAuthenticator.selectedIndex].text;
-    var conjurGlobalJWTSection = document.getElementById('conjurGlobalJWTSection');
-    var conjurLocalAPIKeyCredentials = document.getElementById('conjurLocalAPIKeyCredentials');
-    var conjurLocalTokenClaims = document.getElementById('conjurLocalTokenClaims');
-    var jwtBtn = document.getElementById('jwtValidateButton');
+    const listAuthenticator = document.getElementById('listAuthenticator');
+    const optionText = listAuthenticator.options[listAuthenticator.selectedIndex].text;
+    const conjurGlobalJWTSection = document.getElementById('conjurGlobalJWTSection');
+    const conjurLocalAPIKeyCredentials = document.getElementById('conjurLocalAPIKeyCredentials');
+    const conjurLocalTokenClaims = document.getElementById('conjurLocalTokenClaims');
+    const jwtBtn = document.getElementById('jwtValidateButton');
 
-  if (optionText === "APIKey") {
-        try {
-      conjurGlobalJWTSection.style.display = "none";
-      conjurLocalAPIKeyCredentials.style.display = "block";
-      conjurLocalTokenClaims.style.display = "none";
-      jwtBtn.style.display = "none";
-        } catch (err) {
-        }
+    if (optionText === 'APIKey') {
+        conjurGlobalJWTSection.style.display = 'none';
+        conjurLocalAPIKeyCredentials.style.display = 'block';
+        conjurLocalTokenClaims.style.display = 'none';
+        jwtBtn.style.display = 'none';
     } else if (optionText === 'JWT') {
-        conjurGlobalJWTSection.style.display = "block";
-        conjurLocalAPIKeyCredentials.style.display = "none";
-        conjurLocalTokenClaims.style.display = "block";
-        jwtBtn.style.display = "none";
+        conjurGlobalJWTSection.style.display = 'block';
+        conjurLocalAPIKeyCredentials.style.display = 'none';
+        conjurLocalTokenClaims.style.display = 'block';
+        jwtBtn.style.display = 'none';
     }
 }
 
